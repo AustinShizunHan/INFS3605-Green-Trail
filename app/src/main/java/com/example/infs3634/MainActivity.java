e95350a8b986002ea530640e9daecdc7510bc639
AustinShizunHan <z5271797@unsw.edu.au>
2023-03-14 17:04:05 +1100

    set up

---
+++ app/src/main/java/com/example/infs3634/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.infs3634;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("INFS3605");
+    }
+}
