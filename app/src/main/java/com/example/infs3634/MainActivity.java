120f2b7f31e4cc536bea967c74b9d7343f2f26b4
AustinShizunHan <z5271797@unsw.edu.au>
2023-03-15 14:55:13 +1100

    Working!

--- app/src/main/java/com/example/infs3634/MainActivity.java
+++ app/src/main/java/com/example/infs3634/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Override   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
