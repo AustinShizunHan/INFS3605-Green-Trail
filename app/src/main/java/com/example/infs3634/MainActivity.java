3b3f7f1b6a85621f31875e9f610763b64e9bce4f
AustinShizunHan <z5271797@unsw.edu.au>
2023-03-20 17:55:29 +1100

    map

--- app/src/main/java/com/example/infs3634/MainActivity.java
+++ app/src/main/java/com/example/infs3634/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.infs3634;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    @Override   
+//Implement OnMapReadyCallback.
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the layout file as the content view.
         setContentView(R.layout.activity_main);
-        setTitle("INFS3605");
+
+        // Create a new SupportMapFragment instance
+        SupportMapFragment mapFragment = new SupportMapFragment();
+
+        // Begin the transaction to add the SupportMapFragment to the layout
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+        .add(R.id.map_container, mapFragment)
+        .commit();
+
+        // Set the callback listener for when the map is ready to be used
+        mapFragment.getMapAsync(this);
+    }
+
+
+ //Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
