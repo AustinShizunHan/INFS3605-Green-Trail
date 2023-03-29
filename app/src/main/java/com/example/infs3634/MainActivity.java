def924a2926d0a8d253eaea421794cddc93c7f6f
AustinShizunHan <z5271797@unsw.edu.au>
2023-03-29 21:29:23 +1100

    1

--- app/src/main/java/com/example/infs3634/MainActivity.java
+++ app/src/main/java/com/example/infs3634/MainActivity.java
@@ -3,16 +3,9 @@ package com.example.infs3634;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 //Implement OnMapReadyCallback.
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +13,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Set the layout file as the content view.
         setContentView(R.layout.activity_main);
 
-        // Create a new SupportMapFragment instance
-        SupportMapFragment mapFragment = new SupportMapFragment();
-
-        // Begin the transaction to add the SupportMapFragment to the layout
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-        .add(R.id.map_container, mapFragment)
-        .commit();
-
-        // Set the callback listener for when the map is ready to be used
-        mapFragment.getMapAsync(this);
+//        // Create a new SupportMapFragment instance
+//        SupportMapFragment mapFragment = new SupportMapFragment();
+//
+//        // Begin the transaction to add the SupportMapFragment to the layout
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction()
+//        .add(R.id.map_container, mapFragment)
+//        .commit();
+//
+//        // Set the callback listener for when the map is ready to be used
+//        mapFragment.getMapAsync(this);
     }
 
 
- //Get a handle to the GoogleMap object and display marker.
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
-    }
+        //Get a handle to the GoogleMap object and display marker.
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(0, 0))
+//                .title("Marker"));
+//    }
 }
