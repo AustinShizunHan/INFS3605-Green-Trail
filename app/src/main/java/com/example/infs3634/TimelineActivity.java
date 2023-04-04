9dfa8a74d179739955f1b49dc31dced1e919d935
Learn2Code123 <saanya.bhatnagar@student.unsw.edu.au>
2023-04-04 16:25:52 +1000

    timeline
    i am up to ui

--- app/src/main/java/com/example/infs3634/TimelineActivity.java
+++ app/src/main/java/com/example/infs3634/TimelineActivity.java
@@ -41,22 +41,29 @@ public class TimelineActivity extends AppCompatActivity {
 // method to fill array with data
         public static ArrayList<TimelineModel> getTimelineData() {
             ArrayList<TimelineModel> timelineArrayList = new ArrayList<>();
-            timelineArrayList.add(new TimelineModel("1990s","The Bush Tucker Trail has " +
-                    "been on campus since the 1990s (approx.)" ));
+            timelineArrayList.add(new TimelineModel("1990s","The Bush Tucker Trail (previously known as " +
+                    "the Green Trail) has been on campus since approximately the 1990s." ));
             timelineArrayList.add(new TimelineModel("2000s","UNSW Estate and students " +
-                    "were involved in the early creation, mapping and maintenance of the trail. " ));
-            timelineArrayList.add(new TimelineModel("2007s","The Bush Tucker Trail is opened" +
-                    "for the public. " ));
+                    "were involved in the early creation, mapping and maintenance of the trail." ));
+            timelineArrayList.add(new TimelineModel("2007","The Bush Tucker Trail is opened " +
+                    "for the public." ));
             timelineArrayList.add(new TimelineModel("2021","The Science Sustainability Working Group " +
                     "approves an initial budget to employ students to improve the trail." ));
+
+            timelineArrayList.add(new TimelineModel(" Oct 2021"," The team collaborates" +
+                    " with UNSW Estate Management, UNSW Sustainability, " +
+                    "Nura Gili and students and staff in ADA faculties."));
+
             timelineArrayList.add(new TimelineModel("Nov 2021","A stocktake of the trail " +
-                    "was undertaken by UNSW staff and students …. The name of the trail is changed " +
-                    "from Green Trail to the ‘Bush Tucker Trail’ to better represent Indigenous voice. " ));
-            timelineArrayList.add(new TimelineModel("Oct 2022","A design competition opportunity opens for UNSW students to submit their design for the Bush Tucker Trail. This opportunity will give you a chance to increase awareness and engagement with UNSW Indigenous plants on Kensington Campus. \n" +
-                    "\n" +
-                    "https://www.student.unsw.edu.au/notices/2022/09/logo-design-competition-bush-tucker-trail \n" ));
-            timelineArrayList.add(new TimelineModel("March 2023","Students in INFS3605 are " +
-                    "given the task to help generate ideas for the Bush Tucker Trail by designing and " +
+                    "revealed areas for improvement such as updating signage to raising awareness " +
+                    "and need for represention Indigenous voice. through  " ));
+
+            timelineArrayList.add(new TimelineModel("Oct 2022","A design competition opens for UNSW students " +
+                    "to submit their logo design. Students are given the opportunity " +
+                    "to increase engagement with UNSW Indigenous plants." ));
+
+            timelineArrayList.add(new TimelineModel("March 2023","Students in INFS3605 are" +
+                    "help generate ideas for the Bush Tucker Trail by designing and " +
                     "pitching applications to stakeholders. " ));
             return timelineArrayList;
         }
