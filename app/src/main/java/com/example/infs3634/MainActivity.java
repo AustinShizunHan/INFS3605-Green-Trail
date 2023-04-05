02e7875125adbb082193bc5adce9f927bd3c07aa
AustinShizunHan <z5271797@unsw.edu.au>
2023-04-05 14:32:37 +1000

    Google Map API

--- app/src/main/java/com/example/infs3634/MainActivity.java
+++ app/src/main/java/com/example/infs3634/MainActivity.java
@@ -8,11 +8,17 @@ import androidx.fragment.app.FragmentManager;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 //Implement OnMapReadyCallback.
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+
+    UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +36,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Set the callback listener for when the map is ready to be used
         mapFragment.getMapAsync(this);
+
+
     }
 
 
         //Get a handle to the GoogleMap object and display marker.
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(-33.91001,151.22989660))
-                .title("Marker"));
+    public void onMapReady(GoogleMap map) {
+
+        mMap = map;
+
+        mUiSettings = mMap.getUiSettings();
+
+        //UI settings for Google map
+        mUiSettings.setZoomControlsEnabled(true);
+//        map.setMyLocationEnabled(true);
+//        map.setOnMyLocationButtonClickListener(this);
+//        map.setOnMyLocationClickListener(this);
+
+        //Add Markers
+        final LatLng unswLatLng = new LatLng(-33.917285252570245, 151.22632669113395);
+        Marker unsw = map.addMarker(
+                new MarkerOptions()
+                        .position(unswLatLng)
+                        .title("unsw"));
+        unsw.showInfoWindow();
+
     }
 }
