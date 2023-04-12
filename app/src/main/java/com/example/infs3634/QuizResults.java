088b3801bfa0e8c58f502c91dfdc012a0b5a139e
AustinShizunHan <z5271797@unsw.edu.au>
2023-04-12 14:48:59 +1000

    merging stage 2 not done yet!

---
+++ app/src/main/java/com/example/infs3634/QuizResults.java
@@ -0,0 +1,66 @@
+package com.example.infs3634;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import au.edu.unsw.infs3634.unswlearning.home.HomeDashboardActivity;
+
+
+public class QuizResults extends  AppCompatActivity{
+
+    public static final String INTENT_MESSAGE = "";
+    TextView tvIncorret, tvCorrect, tvFinalResults, greeting;
+    Button restartBtn;
+    Button home;
+
+    @Override
+    // starting new activity
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_results);
+
+        tvIncorret = (TextView) findViewById(R.id.correctresult);
+        tvCorrect = (TextView) findViewById(R.id.incorrectanswer);
+        tvFinalResults = (TextView) findViewById(R.id.tvfinalscore);
+        restartBtn = (Button) findViewById(R.id.btnTryagn);
+        home = (Button) findViewById(R.id.btnHome);
+
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Correct answers:" + QuizActivity.correct + "\n");
+
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong answers:" + QuizActivity.wrong + "\n");
+
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score:" + QuizActivity.correct + "\n");
+
+
+        tvIncorret.setText(sb);
+        tvCorrect.setText(sb2);
+        tvFinalResults.setText(sb3);
+
+
+        QuizActivity.correct = 0;
+        QuizActivity.wrong = 0;
+
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(in);
+            }
+
+        });
+    }};
+
+
+
+
+
---
+++ app/src/main/java/com/example/infs3634/QuizStartPage.java
@@ -0,0 +1,59 @@
+package com.example.infs3634;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class QuizStartPage extends AppCompatActivity {
+
+    final TextView nameText = (TextView) findViewById(R.id.textView);
+    final EditText nametext = (EditText) findViewById(R.id.editName);
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_start);
+
+//mmm
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                // handle Home tab selection
+                return true;
+
+            case R.id.quiz:
+                // handle Search tab selection
+                return true;
+            case R.id.location:
+                // handle Profile tab selection
+                return true;
+            case R.id.timeline:
+        }
+        return false;
+    }
+
+    public void launchQuizActivity(String msg) {
+        Intent intent = new Intent(QuizStartPage.this, QuizActivity.class);
+        intent.putExtra(QuizResults.INTENT_MESSAGE, msg);
+        startActivity(intent);
+        System.out.println("working");
+    }
+
+    public void launchHomeActivity(String msg) {
+        Intent intent = new Intent(QuizStartPage.this, MainActivity.class);
+        intent.putExtra(MainActivity.INTENT_MESSAGE, msg);
+        startActivity(intent);
+    }
+}
